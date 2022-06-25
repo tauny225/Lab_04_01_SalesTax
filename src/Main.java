4c3977f00958588e5cfae9fe001dbb233d79c845
DJEDOU KODJO <tauny225@gmail.com>
2022-06-24 22:08:05 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int Price = 500;
+        double saleTax = 0;
+
+        saleTax = (Price * 5)/100;
+        System.out.println("The computed sale tax of:" + Price + " is " + saleTax);
+
+	// write your code here
+    }
+}
